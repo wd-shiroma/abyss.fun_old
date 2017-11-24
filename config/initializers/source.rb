1c0398f6246860da0c116b56d24c7e638768a80d
wd-shiroma <wonderdream.hts@gmail.com>
2017-11-24 18:50:13 +0900

    ソースコードの参照先を自リポジトリに修正

---
+++ config/initializers/source.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+module Mastodon
+  module Version
+    module_function
+    def source_base_url
+      'https://github.com/wd-shiroma/abyss.fun'
+    end
+  end
+end
+
